3c6b17f85b380a3620abf66aabec31e859f5a1f8
Melory Ayala <melory.ayala@gmail.com>
2023-05-10 15:16:11 +0200

    adding initial structure folders

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+//Defining the engine template
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+//Defining public files
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Able the server to receive data via post (form)
+app.use(express.urlencoded({ extended: true }))
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Title test'
+    })
+})
+
+
+//404 error (not found)
+app.use((req, res) => {
+    res.send('Page not found!')
+})
+
+//Server execution
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
